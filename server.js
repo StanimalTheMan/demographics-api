1e0f2940dbb93b4d57c673652fe276885f3a1c2a
StanimalTheMan <sjc591@nyu.edu>
2021-07-19 15:35:24 -0400

    cinstalled some packages

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const demographics = (req, res) => {
   const { url } = req.body;
